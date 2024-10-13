24338fe07c6aa0d3ccad2382245176b1dc594fb1
ManiKanta <112609309+ManiKanta9578@users.noreply.github.com>
2024-10-13 10:54:00 +0000

    installed dotenv

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const noteRoutes = require('./routes/noteRoutes');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
@@ -11,14 +13,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://manikanta:Mani.rider9@cluster0.fj1hbge.mongodb.net/myNotes')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
 // Routes
 app.use('/api', noteRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
