bb5fb39138fbe6cb526412faa2c40a9e2851ed1b
ManiKanta <manikanta9578@gmail.com>
2024-10-29 17:05:10 +0530

    Responsive layout issue resolved

--- client/components/NoteCard.js
+++ client/components/NoteCard.js
@@ -5,21 +5,21 @@ import 'highlight.js/styles/atom-one-dark.css'; // Choose a theme
 const NoteCard = ({ note }) => {
   return (
     <div className="border border-gray-300 shadow-lg p-4 md:p-6 rounded-lg mx-auto w-full">
-      <h3 className="text-lg md:text-2xl lg:text-3xl font-semibold mb-3 text-left">{note.question}</h3>
+      <h3 className="text-base font-semibold mb-3 text-left">{note.question}</h3>
       {note.content.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 pl-0 md:pl-8 lg:pl-8">
           {note.content.map((item, index) => {
             if (item.type === "answer") {
               return (
                 <div
                   key={index}
-                  className="mb-2 text-lg md:text-base lg:text-lg"
+                  className="mb-2 text-base"
                   dangerouslySetInnerHTML={{ __html: item.value }}
                 />
               );
             } else if (item.type === "code") {
               return (
-                <div key={index} className="mb-2 text-xs">
+                <div key={index} className="mb-2 text-sm">
                   <Highlight> {item.value} </Highlight>
                 </div>
               );
